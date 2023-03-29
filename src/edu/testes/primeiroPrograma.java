30fc843e1f916e7f4413006a5060061f39266a73
maxweberps <max.ps0235@gmail.com>
2023-03-29 15:23:26 -0300

    primeiro commit

---
+++ src/edu/testes/primeiroPrograma.java
@@ -0,0 +1,13 @@
+package edu.testes;
+
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        System.out.printf("Oi mundo!\n");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
