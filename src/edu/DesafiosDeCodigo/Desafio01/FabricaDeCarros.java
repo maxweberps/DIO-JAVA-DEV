f465718979cf324d9b8bdc6d702f3ec29fc05103
maxweberps <max.ps0235@gmail.com>
2023-03-29 17:11:07 -0300

    codigo final

--- src/edu/DesafiosDeCodigo/Desafio01/FabricaDeCarros.java
+++ src/edu/DesafiosDeCodigo/Desafio01/FabricaDeCarros.java
@@ -1,4 +1,21 @@
 package edu.DesafiosDeCodigo.Desafio01;
 
+import java.util.Scanner;
+
 public class FabricaDeCarros {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int custoFabrica = scan.nextInt();
+        int percentualDistribuidor = scan.nextInt();
+        int percentualImpostos = scan.nextInt();
+
+        int custoImpostos;
+        custoImpostos = (custoFabrica * percentualImpostos) / 100;
+        int custoDistribuidor;
+        custoDistribuidor = (custoFabrica * percentualDistribuidor) / 100;
+
+        System.out.println("Custo do carro novo: "+(custoFabrica + custoImpostos + custoDistribuidor));
+
+
+    }
 }
